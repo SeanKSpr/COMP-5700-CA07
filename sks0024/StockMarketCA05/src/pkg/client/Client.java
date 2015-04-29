46c4f740292ac220291aeb3209504f12bdca0ecb
Sean Spurlin <sks0024@auburn.edu>
2015-04-29 00:15:50 -0500

    Suggestions taking into consideration Renamed m to marketNASDAQ Altered incorrect comment Removed TODO comment

--- sks0024/StockMarketCA05/src/pkg/client/Client.java
+++ sks0024/StockMarketCA05/src/pkg/client/Client.java
@@ -8,13 +8,13 @@ import pkg.trader.Trader;
 
 public class Client {
 	public static void main(String[] args) {
-		Market m = new Market("NASDAQ");
-		IPO.enterNewStock(m, "SBUX", "Starbucks Corp.", 92.86);
-		IPO.enterNewStock(m, "TWTR", "Twitter Inc.", 47.88);
-		IPO.enterNewStock(m, "VSLR", "Vivint Solar", 16.44);
-		IPO.enterNewStock(m, "GILD", "Gilead Sciences", 93.33);
+		Market marketNASDAQ = new Market("NASDAQ");
+		IPO.enterNewStock(marketNASDAQ, "SBUX", "Starbucks Corp.", 92.86);
+		IPO.enterNewStock(marketNASDAQ, "TWTR", "Twitter Inc.", 47.88);
+		IPO.enterNewStock(marketNASDAQ, "VSLR", "Vivint Solar", 16.44);
+		IPO.enterNewStock(marketNASDAQ, "GILD", "Gilead Sciences", 93.33);
 
-		m.printStocks();
+		marketNASDAQ.printStocks();
 
 		Market m1 = new Market("Nikkei");
 		IPO.enterNewStock(m1, "BABA", "Alibaba", 84.88);
@@ -22,7 +22,7 @@ public class Client {
 
 		m1.printStocks();
 
-		// Create 10 traders
+		// Create 14 traders
 		Trader trader1 = new Trader("Neda", 200000.00);
 		Trader trader2 = new Trader("Scott", 100000.00);
 		Trader trader3 = new Trader("Luke", 100000.00);
@@ -43,18 +43,17 @@ public class Client {
 		Trader trader16 = new Trader("T2", 300000.00);
 
 		try {
-			trader1.buyFromBank(m, "SBUX", 1600);
-			trader2.buyFromBank(m, "SBUX", 300);
-			trader3.buyFromBank(m, "SBUX", 300);
-			trader4.buyFromBank(m, "SBUX", 300);
-			trader5.buyFromBank(m, "SBUX", 600);
-			trader6.buyFromBank(m, "SBUX", 700);
-			trader7.buyFromBank(m, "SBUX", 500);
-			trader15.buyFromBank(m, "SBUX", 1500);
+			trader1.buyFromBank(marketNASDAQ, "SBUX", 1600);
+			trader2.buyFromBank(marketNASDAQ, "SBUX", 300);
+			trader3.buyFromBank(marketNASDAQ, "SBUX", 300);
+			trader4.buyFromBank(marketNASDAQ, "SBUX", 300);
+			trader5.buyFromBank(marketNASDAQ, "SBUX", 600);
+			trader6.buyFromBank(marketNASDAQ, "SBUX", 700);
+			trader7.buyFromBank(marketNASDAQ, "SBUX", 500);
+			trader15.buyFromBank(marketNASDAQ, "SBUX", 1500);
 			// Exception
-			trader8.buyFromBank(m, "SBUX", 5000);
+			trader8.buyFromBank(marketNASDAQ, "SBUX", 5000);
 		} catch (StockMarketExpection e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		trader1.printTrader();
@@ -69,14 +68,14 @@ public class Client {
 
 		// Place sell orders
 		try {
-			trader1.placeNewOrder(m, "SBUX", 100, 97.0, OrderType.SELL);
-			trader2.placeNewOrder(m, "SBUX", 300, 97.5, OrderType.SELL);
-			trader3.placeNewOrder(m, "SBUX", 300, 98.0, OrderType.SELL);
-			trader4.placeNewOrder(m, "SBUX", 300, 98.5, OrderType.SELL);
-			trader5.placeNewOrder(m, "SBUX", 500, 99.0, OrderType.SELL);
-			trader6.placeNewOrder(m, "SBUX", 700, 99.5, OrderType.SELL);
-			trader7.placeNewOrder(m, "SBUX", 500, 100.0, OrderType.SELL);
-			trader15.placeNewMarketOrder(m, "SBUX", 1500, 0, OrderType.SELL);
+			trader1.placeNewOrder(marketNASDAQ, "SBUX", 100, 97.0, OrderType.SELL);
+			trader2.placeNewOrder(marketNASDAQ, "SBUX", 300, 97.5, OrderType.SELL);
+			trader3.placeNewOrder(marketNASDAQ, "SBUX", 300, 98.0, OrderType.SELL);
+			trader4.placeNewOrder(marketNASDAQ, "SBUX", 300, 98.5, OrderType.SELL);
+			trader5.placeNewOrder(marketNASDAQ, "SBUX", 500, 99.0, OrderType.SELL);
+			trader6.placeNewOrder(marketNASDAQ, "SBUX", 700, 99.5, OrderType.SELL);
+			trader7.placeNewOrder(marketNASDAQ, "SBUX", 500, 100.0, OrderType.SELL);
+			trader15.placeNewMarketOrder(marketNASDAQ, "SBUX", 1500, 0, OrderType.SELL);
 
 		} catch (StockMarketExpection e) {
 			// TODO Auto-generated catch block
@@ -96,14 +95,14 @@ public class Client {
 
 		// Place buy orders
 		try {
-			trader8.placeNewOrder(m, "SBUX", 200, 101.0, OrderType.BUY);
-			trader9.placeNewOrder(m, "SBUX", 300, 100.5, OrderType.BUY);
-			trader10.placeNewOrder(m, "SBUX", 400, 100.0, OrderType.BUY);
-			trader11.placeNewOrder(m, "SBUX", 500, 99.5, OrderType.BUY);
-			trader12.placeNewOrder(m, "SBUX", 900, 99.0, OrderType.BUY);
-			trader13.placeNewOrder(m, "SBUX", 1000, 98.5, OrderType.BUY);
-			trader14.placeNewOrder(m, "SBUX", 900, 98.0, OrderType.BUY);
-			trader16.placeNewMarketOrder(m, "SBUX", 700, 0, OrderType.BUY);
+			trader8.placeNewOrder(marketNASDAQ, "SBUX", 200, 101.0, OrderType.BUY);
+			trader9.placeNewOrder(marketNASDAQ, "SBUX", 300, 100.5, OrderType.BUY);
+			trader10.placeNewOrder(marketNASDAQ, "SBUX", 400, 100.0, OrderType.BUY);
+			trader11.placeNewOrder(marketNASDAQ, "SBUX", 500, 99.5, OrderType.BUY);
+			trader12.placeNewOrder(marketNASDAQ, "SBUX", 900, 99.0, OrderType.BUY);
+			trader13.placeNewOrder(marketNASDAQ, "SBUX", 1000, 98.5, OrderType.BUY);
+			trader14.placeNewOrder(marketNASDAQ, "SBUX", 900, 98.0, OrderType.BUY);
+			trader16.placeNewMarketOrder(marketNASDAQ, "SBUX", 700, 0, OrderType.BUY);
 		} catch (StockMarketExpection e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -120,7 +119,7 @@ public class Client {
 		trader14.printTrader();
 		trader16.printTrader();
 
-		m.triggerTrade();
+		marketNASDAQ.triggerTrade();
 
 		System.out.println("Printing after the tradings are done");
 		System.out.println("&^&^&^&^&^&^&^&^&^&^&^&^&^&^&^&^&^&^&^&^");
@@ -141,7 +140,7 @@ public class Client {
 		trader15.printTrader();
 		trader16.printTrader();
 
-		m.printHistoryFor("SBUX");
+		marketNASDAQ.printHistoryFor("SBUX");
 
 	}
 }
